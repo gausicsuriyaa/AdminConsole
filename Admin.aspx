f6ab7172de40eab136d9a81df1c2dfc9fb409782
V146912 <suriyaa08@gmail.com>
2015-08-27 08:45:44 -0700

    Initial Commit

---
+++ Admin.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Admin.aspx.cs" Inherits="Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <asp:PlaceHolder runat="server">
+        <script src="<%: ResolveUrl("~/Scripts/modernizr-2.5.3.js") %>"></script>
+    </asp:PlaceHolder>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <link href="Content/bootstrap.cosmo.min.css" rel="stylesheet" />
+
+    <style>
+        .ui-autocomplete {
+            font-size: 11px;
+            text-align: left;
+        }
+    </style>
+    <script type="text/javascript">
+        $(function () {
+            $('#<%=txtQueueList.ClientID%>').autocomplete({
+                minLength: 2,
+                source: function (request, response) {
+                    $.ajax({
+                        url: "Admin.aspx/GetAgentList",
+                        data: "{ 'pre':'" + request.term + "'}",
+                        dataType: "json",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        success: function (data) {
+                            response($.map(data.d, function (item) {
+                                return { value: item }
+                            }))
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            alert(textStatus);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="shadowBox">
+            <div class="page-container">
+                <div class="container">
+                    <div class="jumbotron">
+                        <p class="text-danger">Agent Spare Time Optimization Watcher </p>
+                        <span class="text-info">Agent Call Status per Queue</span>
+                    </div>
+                    <div>
+                        <br />
+                        <span class="text-info">Select a Queue Name</span>
+                        <asp:TextBox ID="txtQueueList" runat="server" Width="350px" CssClass="textboxAuto" Font-Size="13px" OnTextChanged="txtQueueList_TextChanged"></asp:TextBox>
+                        <br /><br />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 ">
+                    <div class="table-responsive">
+                        <asp:GridView ID="grdAgentInfo" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover" AutoGenerateColumns="False" DataKeyNames="Agent" EmptyDataText="There are no data records to display.">
+                            <Columns>
+                                <asp:BoundField DataField="Agent" HeaderText="Agent Name" ReadOnly="True" SortExpression="AgentName" />
+                                <asp:BoundField DataField="Status" HeaderText="Status Of the Agent" SortExpression="Status" />
+                            </Columns>
+                        </asp:GridView>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </form>
+</body>
+</html>
---
+++ Admin.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.ComponentModel;
+
+public partial class Admin : System.Web.UI.Page
+{
+    private static List<string> Agents = new List<string>();
+    private static List<string> Queues = new List<string>();
+    private static List<AgentQueueInfo> AgentQueueData = new List<AgentQueueInfo>();
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            AgentQueueData = LoadAgentInfo(@"~/AgentInfo.xml");
+
+            foreach (AgentQueueInfo item in AgentQueueData)
+            {
+                Agents.Add(item.Agent);
+                Queues.Add(item.Queue);
+            }
+
+            Agents = Agents.Distinct().ToList();
+            Queues = LoadQueues(Queues);
+        }
+    }
+
+    private List<AgentQueueInfo> LoadAgentInfo (string xmlPath)
+    {
+        List<AgentQueueInfo> agentQueueInfo = new List<AgentQueueInfo>();
+        
+        XDocument agentQueueDoc = XDocument.Load(Server.MapPath(xmlPath));
+        IEnumerable<XElement> agentElement = agentQueueDoc.Descendants("Agent");
+
+        foreach (XElement item in agentElement)
+        {
+            AgentQueueInfo tempagent = new AgentQueueInfo
+            {
+                Agent = item.Attribute("id").Value.ToString(),
+                Queue = item.Attribute("queues").Value.ToString(),
+                Status = item.Attribute("status").Value.ToString()
+            };
+            agentQueueInfo.Add(tempagent);
+        }
+        return agentQueueInfo;
+    }
+
+    private List<string> LoadQueues (List<string> QueueList)
+    {
+        List<string> queueInfo = new List<string>();
+
+        foreach (string item in QueueList)
+        {
+            string[] queueSplit = item.Split(',');
+            foreach (string splitItem in queueSplit)
+            {
+                queueInfo.Add(splitItem);
+            }
+        }
+        
+        queueInfo = queueInfo.Distinct().ToList();
+        return queueInfo;
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static List<string> GetAgentList(string pre)
+    {
+        List<string> agentList = new List<string>();
+        foreach (string item in Queues)
+        {
+
+            if (item.StartsWith(pre))
+            {
+                agentList.Add(item);
+            }
+        }
+        return agentList;
+    }
+
+    protected void txtQueueList_TextChanged(object sender, EventArgs e)
+    {
+        string queueSelected = txtQueueList.Text.ToString();
+        List<AgentData> agentDataforQueue = LoadAgentDataforQueue(queueSelected);
+
+        DataTable grdAgent = ConvertToDataTable(agentDataforQueue);
+        grdAgentInfo.DataSource = grdAgent;
+        grdAgentInfo.DataBind();
+    }
+
+    private List<AgentData> LoadAgentDataforQueue(string queueName)
+    {
+        List<AgentData> agentData = new List<AgentData>();
+         var agentStatusData = (from agentsforqueue in AgentQueueData
+                     where agentsforqueue.Queue.Split(',').Contains(queueName)
+                     select new { Agent = agentsforqueue.Agent, Status = agentsforqueue.Status });
+
+         foreach (var item in agentStatusData)
+         {
+             AgentData tempData = new AgentData { Agent = item.Agent, Status = item.Status };
+             agentData.Add(tempData);
+         }
+
+         return agentData;
+    }
+
+    public DataTable ConvertToDataTable<T>(IList<T> data)
+    {
+        PropertyDescriptorCollection properties =
+           TypeDescriptor.GetProperties(typeof(T));
+        DataTable table = new DataTable();
+        foreach (PropertyDescriptor prop in properties)
+            table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+        foreach (T item in data)
+        {
+            DataRow row = table.NewRow();
+            foreach (PropertyDescriptor prop in properties)
+                row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+            table.Rows.Add(row);
+        }
+        return table;
+
+    }
+    
+}
+
+public class AgentQueueInfo
+{
+    public string Agent { get; set; }
+    public string Queue { get; set; }
+    public string Status { get; set; }
+}
+
+public class AgentData
+{
+    public string Agent { get; set; }
+    public string Status { get; set; }
+}
